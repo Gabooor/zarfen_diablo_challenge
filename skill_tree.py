8521be84b00c4a302dc895712459c412b68ae323
Fábián Gábor <gaborfabian11@gmail.com>
2024-09-02 12:50:31 +0200

    created skill tree class

---
+++ skill_tree.py
@@ -0,0 +1,12 @@
+class Skill_Tree:
+    def __init__(self, name, character_class, skills, layout):
+        self.character_class = character_class
+        self.name = name
+        self.skills = skills
+        self.layout = layout
+
+    def __repr__(self):
+        skills_repr = ', '.join(f"{skill.name!r}" for skill in self.skills)
+        return (f"Skill_Tree(character_class={self.character_class!r}, "
+                f"name={self.name!r}, "
+                f"skills=[{skills_repr}])")
